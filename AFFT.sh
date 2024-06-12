6187814169bb508bb3ea78e85cdb7deec4442cc1
Kareem Ebrahem <safen894@gmail.com>
2024-06-12 15:57:32 +0300

    V4.0.0

--- AFFT.sh
+++ AFFT.sh
@@ -10,14 +10,14 @@ ICAKL19fLyAgICAgXF9fXCB8X198ICAgICB8X198ICAgICAgICAgfF9ffCAgICAgCiAgICAgICAg
 ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAo="
 
 echo -e "\033[91mWelcome to AFFT Tool\033[0m"
-echo -e "\033[93mVersion: 3.1.2\033[0m"
+echo -e "\033[93mVersion: 4.0.0\033[0m"
 echo -e "\033[32mCreated By: MRX7014\033[0m"
 
 sleep 1
 
 echo ""
 
-echo -e "\033[93m(1) => Install Flagships Features\033[0m"
+echo -e "\033[93m(1) => Features\033[0m"
 echo -e "\033[93m(2) => APPS\033[0m"
 echo -e "\033[93m(3) => Debloater\033[0m"
 echo -e "\033[93m(4) => Exit\033[0m"
@@ -25,6 +25,34 @@ echo ""
 read -p  "Choose: " user
 
 if [ $user == 1 ]
+then
+
+base64 -d <<<"CiAgX19fX19fIF8gICAgICAgICAgICAgICAgIF8gICAgIF8gICAgICAgICAgIAogfCAgX19fX3wg
+fCAgICAgICAgICAgICAgIHwgfCAgIChfKSAgICAgICAgICAKIHwgfF9fICB8IHwgX18gXyAgX18g
+XyBfX198IHxfXyAgXyBfIF9fICBfX18gCiB8ICBfX3wgfCB8LyBfYCB8LyBfYCAvIF9ffCAnXyBc
+fCB8ICdfIFwvIF9ffAogfCB8ICAgIHwgfCAoX3wgfCAoX3wgXF9fIFwgfCB8IHwgfCB8XykgXF9f
+IFwKIHxffCAgICB8X3xcX18sX3xcX18sIHxfX18vX3wgfF98X3wgLl9fL3xfX18vCiAgICAgICAg
+ICAgICAgICAgIF9fLyB8ICAgICAgICAgICB8IHwgICAgICAgIAogIF9fX19fXyAgICAgICAgIHxf
+X18vICAgICAgICAgICAgfF98ICAgICAgICAKIHwgIF9fX198ICAgICAgIHwgfCAgICAgICAgICAg
+ICAgICAgICAgICAgICAgCiB8IHxfXyBfX18gIF9fIF98IHxfIF8gICBfIF8gX18gX19fICBfX18g
+ICAgIAogfCAgX18vIF8gXC8gX2AgfCBfX3wgfCB8IHwgJ19fLyBfIFwvIF9ffCAgICAKIHwgfCB8
+ICBfXy8gKF98IHwgfF98IHxffCB8IHwgfCAgX18vXF9fIFwgICAgCiB8X3wgIFxfX198XF9fLF98
+XF9ffFxfXyxffF98ICBcX19ffHxfX18vICAgIAogICAgICAgICAgICAgICAgICAgICAgICAgICAg
+ICAgICAgICAgICAgICAgICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+ICAgICAgCg=="
+
+echo ""
+
+echo -e "\033[93m(1) => Flagships Features\033[0m"
+echo -e "\033[93m(2) => AI Features\033[0m"
+echo -e "\033[93m(3) => S24 Ultra Device Info\033[0m"
+echo -e "\033[93m(4) => Back To Main Menu\033[0m"
+echo ""
+read -p  "Choose: " userfeatures
+
+fi
+
+if [ $userfeatures == 1 ]
 then
 
 echo ""
@@ -103,7 +131,7 @@ echo -e "\033[31mNow Will Add S23U Props Magisk Module to AFFT_FILES Folder, You
 
 mkdir /sdcard/AFFT_FILES && cp features/S23UProps/S23U_UP1A.231005.007.zip /sdcard/AFFT_FILES
 
-sleep 3
+sleep 5
 
 echo ""
 
@@ -112,6 +140,51 @@ echo -e "\033[93mNow everything is good, You should install S23U Props Magisk Mo
 sleep 5
 
 clear ; bash AFFT.sh
+
+fi
+
+if [ $userfeatures == 2 ]
+then
+
+echo ""
+
+
+echo""
+
+read -p "Choose: " useraifeatures
+
+fi
+
+if [ $useraifeatures == 1 ]
+then
+
+echo ""
+
+echo -e "\033[34mWill Copy AI Wallpaper App On AFFT_FILES On SdCard, Check it\033[0m"
+
+sleep 2
+
+mkdir /sdcard/AFFT_FILES cp Apps/AI-Wallpaper/*.apk /sdcard/AFFT_FILES > /dev/null 2>&1
+
+sleep 3
+
+echo ""
+
+echo "Done"
+
+fi
+
+if [ $userfeatures == 3 ]
+then
+
+bash features/Device-Info.sh
+
+fi
+
+if [ $userfeatures == 4 ]
+then
+
+bash AFFT.sh
 
 fi
 

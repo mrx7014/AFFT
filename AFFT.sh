89b2b5cda54fdf097e57789fa8bff6a4534ce9e2
Kareem Ebrahem <safen894@gmail.com>
2024-06-12 20:38:36 +0300

    V4.0.0 - Stable (Fix Animations Commands Bugs)

--- AFFT.sh
+++ AFFT.sh
@@ -134,11 +134,11 @@ sudo settings put global window_animation_scale 0.7
 
 sleep 1
 
-settings put global transition_animation_scale 0.7
+sudo settings put global transition_animation_scale 0.7
 
 sleep 1
 
-settings put global animator_duration_scale 0.7
+sudo settings put global animator_duration_scale 0.7
 
 sleep 1
 

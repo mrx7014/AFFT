dc373575eee24866dc3acb352212a35ced9c3726
MRX7014 - UZI <safen894@gmail.com>
2024-06-23 17:05:29 +0300

    Update AFFT.sh

--- AFFT.sh
+++ AFFT.sh
@@ -48,156 +48,42 @@ echo ""
 echo -e "\033[93m(1) => Flagships Features\033[0m"
 echo -e "\033[93m(2) => Galaxy AI\033[0m"
 echo -e "\033[93m(3) => S24 Ultra Device Info\033[0m"
-echo -e "\033[93m(0) => Exit\033[0m"
+echo -e "\033[93m(0) => Back To Main Menu\033[0m"
 echo ""
 read -p  "Choose: " userfeatures
-
 fi
 
 if [ $userfeatures == 1 ]
 then
-
 bash features/FloatingFeatures.sh
 fi
 
 if [ $userfeatures == 2 ]
 then
-
-clear
-
-base64 -d <<<"CiAgIF9fX19fICAgICAgIF8gICAgICAgICAgICAgICAgICAKICAvIF9fX198ICAgICB8IHwgICAg
-ICAgICAgICAgICAgIAogfCB8ICBfXyAgX18gX3wgfCBfXyBfX18gIF9fXyAgIF8gCiB8IHwgfF8g
-fC8gX2AgfCB8LyBfYCBcIFwvIC8gfCB8IHwKIHwgfF9ffCB8IChffCB8IHwgKF98IHw+ICA8fCB8
-X3wgfAogIFxfX19fX3xcX18sX3xffFxfXyxfL18vXF9cXF9fLCB8CiAgICAgICAgICAgX19fX18g
-ICAgICAgICAgICAgX18vIHwKICAgICAvXCAgIHxfICAgX3wgICAgICAgICAgIHxfX18vIAogICAg
-LyAgXCAgICB8IHwgICAgICAgICAgICAgICAgICAgCiAgIC8gL1wgXCAgIHwgfCAgICAgICAgICAg
-ICAgICAgICAKICAvIF9fX18gXCBffCB8XyAgICAgICAgICAgICAgICAgIAogL18vICAgIFxfXF9f
-X19ffCAgICAgICAgICAgICAgICAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAK
-ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAo="
-echo""
-
-sleep 1
-
-echo -e "\033[31mChoose App You Want To Install\033[0m"
-
-echo ""
-
-echo -e "\033[31m(1) AI Wallpaper\033[0m"
-
-echo ""
-
-echo -e "\033[31m(0) Exit\033[0m"
-echo""
-
-read -p "Choose: " useraifeatures
-
-fi
-
-if [ $useraifeatures == 1 ]
-then
-
-echo ""
-
-echo -e "\033[34mWill Copy AI Wallpaper App On AFFT_FILES On SdCard, Check it\033[0m"
-
-sleep 2
-
-mkdir /sdcard/AFFT_FILES cp Apps/AI-Wallpaper/*.apk /sdcard/AFFT_FILES > /dev/null 2>&1
-
-sleep 3
-
-echo ""
-
-echo "Done"
-
-sleep 1
-
-echo "The Tool Will Close Now, Run it again type (bash AFFT.sh)"
-
-sleep 1
-
-exit
-
-fi
-
-if [ $useraifeatures == 0 ]
-then
-
-echo "The Tool Will Close Now, Run it again type (bash AFFT.sh)"
-
-sleep 1
-
-exit
-
+bash features/GalaxyAI.sh
 fi
 
 if [ $userfeatures == 3 ]
 then
-
 bash features/Device-Info.sh
-
 fi
 
 if [ $userfeatures == 0 ]
 then
-
-echo "The Tool Will Close Now, Run it again type (bash AFFT.sh)"
-
-sleep 1
-
-exit
-
+clear ; bash AFFT.sh
 fi
 
 if [ $user == 2 ]
 then
-
 bash features/Debloater.sh
-
 fi
 
 if [ $user == 3 ]
 then
-
-clear
-
-base64 -d <<<"CiAgIF9fXyAgICAgICAgICBfXyAgICAgICAgICAgICAgCiAgLyBfIFxfX18gX19fIC8gL19fX18g
-IF9fX19fX18gCiAvICwgXy8gLV98Xy08LyBfXy8gXyBcLyBfXy8gLV8pCi9fL3xffFxfXy9fX18v
-XF9fL1xfX18vXy9fX1xfXy8gCiAgLyBfIFxfX18gLyBfL19fIF9fXyBfXy8gLyAvXyAgCiAvIC8v
-IC8gLV8pIF8vIF8gYC8gLy8gLyAvIF9fLyAgCi9fX19fL1xfXy9fLyBcXyxfL1xfLF8vXy9cX18v
-ICAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCg=="
-
-echo "Will Delete All AFFT Changes And Restore Default Files (buildprop,floating-features,camera-features)"
-
-sleep 2
-
-sudo mount -o rw,remount / && sudo mount -o rw,remount / /dev/null 2>&1
-
-sudo cp features/RestoreDefault/build.prop /system && sudo cp features/BuildProp/build.prop /system > /dev/nill 2>&1
-
-sleep 1
-
-sudo cp features/RestoreDefault/floating_feature.xml /system/etc && sudo cp features/FloatingFeatures/floating_feature.xml /system/etc > /dev/null 2>&1
-
-sleep 1
-
-sudo cp features/RestoreDefault/camera-feature.xml /system/cameradata && sudo cp features/CameraFeatures/camera-feature.xml /system/cameradata > /dev/null 2>&1
-
-sleep 3
-
-echo "Done"
-
-echo "The Tool Will Close Now, Run it again type (bash AFFT.sh)"
-
-sleep 1
-
-exit
-
+bash features/RestoreDefault.sh
 fi
 
 if [ $user == 4 ]
 then
-
 clear ; exit
-
 fi
